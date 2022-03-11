09c9437f86bc3c3fd65cbbc99105cb11248bdb47
salih101 <95134518+salih101@users.noreply.github.com>
2022-03-11 22:42:24 +0530

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<torch_light/TorchLightPlugin.h>)
+#import <torch_light/TorchLightPlugin.h>
+#else
+@import torch_light;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [TorchLightPlugin registerWithRegistrar:[registry registrarForPlugin:@"TorchLightPlugin"]];
+}
+
+@end
